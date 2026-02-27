674069e2a3942d435e9c5b1cb21b5aead0364f40
Zaden Yip <hyfrencn@gmail.com>
2026-02-27 22:54:39 +0800

    chore(indentation): fix indentation in meaning-card.component.ts

--- src/app/shared/dictionary/sub-components/meaning-card.component.ts
+++ src/app/shared/dictionary/sub-components/meaning-card.component.ts
@@ -6,27 +6,22 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { Definition } from '../dictionary-interface';
 
 @Component({
-  selector: 'app-meaning-card',
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatCardModule,
-    MatExpansionModule
-  ],
-  templateUrl: './meaning-card.component.html',
-  styleUrl: './meaning-card.component.scss',
+    selector: 'app-meaning-card',
+    imports: [CommonModule, MatButtonModule, MatCardModule, MatExpansionModule],
+    templateUrl: './meaning-card.component.html',
+    styleUrl: './meaning-card.component.scss',
 })
 export class MeaningCardComponent {
-  index = input(0);
-  posLabel = input('');
-  item = input<Definition>({
-    cefr: '',
-    definition: { source: '', target: '' },
-    examples: [],
-  });
-  add = output<Definition>();
+    index = input(0);
+    posLabel = input('');
+    item = input<Definition>({
+        cefr: '',
+        definition: { source: '', target: '' },
+        examples: [],
+    });
+    add = output<Definition>();
 
-  addToCard(): void {
-    this.add.emit(this.item());
-  }
+    addToCard(): void {
+        this.add.emit(this.item());
+    }
 }
