e6c72abff2edfec1e2025464cc91d0f434dee753
Zaden Yip <hyfrencn@gmail.com>
2026-04-25 00:52:46 +0800

    complete base page for collection config

--- client/src/app/settings/collection-config/col-config.component.ts
+++ client/src/app/settings/collection-config/col-config.component.ts
@@ -1,9 +1,9 @@
-import { Component } from "@angular/core";
-import { CollectionConfig } from "@main/db/services/repetition/collection/col-service-types";
+﻿import { Component } from "@angular/core";
+import { DailyResetTimeComponent } from "./sub/reset-time.component";
 
 @Component({
     selector: "app-collection-config",
-    imports: [],
+    imports: [DailyResetTimeComponent],
     templateUrl: "./col-config.component.html",
     styleUrl: "./col-config.component.scss",
 })
---
+++ client/src/app/settings/collection-config/sub/reset-time.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { TranslateModule } from "@ngx-translate/core";
+
+@Component({
+    selector: "app-collection-daily-reset-time",
+    imports: [TranslateModule, MatCardModule, MatFormFieldModule, MatInputModule, FormsModule],
+    templateUrl: "./reset-time.component.html",
+    styleUrl: "../../mat-card.scss",
+})
+export class DailyResetTimeComponent {
+    dailyResetTime = 4;
+
+    /**
+     * keep the input value between 0 and 23.
+     * Non-integer input will be prevented by the input tag's attributes
+     * @param _event
+     */
+    onDailyResetTimeInput(event: Event): void {
+        const inputEl = event.target as HTMLInputElement;
+        const raw = Number(inputEl.value);
+
+        if (Number.isNaN(raw)) {
+            return;
+        }
+
+        const hour = Math.max(0, Math.min(23, Math.trunc(raw)));
+        inputEl.value = hour.toString();
+        this.dailyResetTime = hour;
+    }
+}
