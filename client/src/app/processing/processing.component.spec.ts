6846407210a4ae52ac90b36c2964cf855fb8db02
HyFren <hyfrencn@gmail.com>
2026-05-04 22:55:07 +0800

    add TranslateModule to processing.component.spec.ts for passing tests

--- client/src/app/processing/processing.component.spec.ts
+++ client/src/app/processing/processing.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ProcessingComponent } from "./processing.component";
+import { TranslateModule } from "@ngx-translate/core";
 
 describe("ProcessingComponent", () => {
     let component: ProcessingComponent;
@@ -8,7 +9,7 @@ describe("ProcessingComponent", () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [ProcessingComponent],
+            imports: [ProcessingComponent, TranslateModule.forRoot()],
         }).compileComponents();
 
         fixture = TestBed.createComponent(ProcessingComponent);
