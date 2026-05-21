24928e51637f2ae206717b6ee19c50d01950a717
HyFren <hyfrencn@gmail.com>
2026-05-22 01:21:29 +0800

    add card-service-helper and card-service-types

---
+++ client/app/db/services/repetition/cards/card-service-types.d.ts
@@ -0,0 +1,119 @@
+import { Grade } from "ts-fsrs";
+
+export interface CardRef {
+    /**
+     * Card primary id in hex string format.
+     */
+    id: string;
+}
+
+export interface LangCard {
+    /**
+     * Card primary id in hex string format.
+     */
+    id: string;
+    /**
+     * Owner note id in hex string format.
+     */
+    noteId: string;
+    /**
+     * Owner deck id in hex string format.
+     */
+    deckId: string;
+    /**
+     * Sync sequence number.
+     */
+    usn: number;
+    /**
+     * Last updated timestamp in milliseconds.
+     */
+    updatedAt: number;
+
+    /**
+     * corresponding note template's card template business id
+     */
+    cardTemplateId: number;
+
+    /**
+     * FSRS difficulty.
+     */
+    difficulty: number;
+    /**
+     * FSRS stability.
+     */
+    stability: number;
+    /**
+     * current card interval in days
+     */
+    scheduledDays: number;
+
+    /**
+     * Next due timestamp.
+     */
+    due: Date;
+    /**
+     * Last review timestamp.
+     */
+    lastReview?: Date;
+    /**
+     * Lapse count.
+     */
+    lapses: number;
+    /**
+     * Current learning step index.
+     */
+    learningSteps: number;
+    /**
+     * repetition count
+     */
+    repetitions: number;
+    /**
+     * 0 = new, 1 = learning, 2 = review, 3 = relearning
+     */
+    state: number;
+    /**
+     * -1 = suspended, 0, 1, 2, 3 same as state
+     */
+    queue: number;
+}
+
+export type FSRSCard = Pick<
+    LangCard,
+    | "difficulty"
+    | "stability"
+    | "scheduledDays"
+    | "due"
+    | "lastReview"
+    | "lapses"
+    | "learningSteps"
+    | "repetitions"
+    | "state"
+>
+
+export interface FSRSReviewLog {
+    reviewTime: Date;
+    scheduledDays: number;
+    rating: number;
+    difficulty: number;
+    stability: number;
+    learningSteps: number;
+    state: number;
+}
+
+export interface LangReviewLog extends FSRSReviewLog {
+    id: string;
+    cardId: string;
+    usn: number;
+    duration: number;
+}
+
+export interface FSRSRecordLogItem {
+    card: FSRSCard;
+    log: FSRSReviewLog;
+}
+
+export type FSRSRecordLog = Record<Grade, FSRSRecordLogItem>;
+
+export interface FSRSIPreview extends FSRSRecordLog {
+    [Symbol.iterator](): IterableIterator<FSRSRecordLogItem>;
+}
